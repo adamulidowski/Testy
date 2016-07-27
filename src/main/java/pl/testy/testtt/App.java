d52a3d30d661f30d756b49a5a0875ac0cfaf5031
Adam Ulidowski <adam.ulidowski@wp.pl>
2016-07-27 10:12:54 +0200

    Proba zlaczenia

--- src/main/java/pl/testy/testtt/App.java
+++ src/main/java/pl/testy/testtt/App.java
@@ -10,6 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "Zmiany");
-        System.out.println( "Proba zlaczenia");
+        System.out.println( "Proba zlaczeniaa");
     }
 }
